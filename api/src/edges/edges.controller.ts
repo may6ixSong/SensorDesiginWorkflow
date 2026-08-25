e983f848261200a6bf641c79ae6d036de594ddf7
may6ixSong <138551769+may6ixSong@users.noreply.github.com>
2026-08-25 10:37:41 +0900

    Update edges.controller.ts

--- api/src/edges/edges.controller.ts
+++ api/src/edges/edges.controller.ts
@@ -1,10 +1,9 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { IpAccessGuard } from '../common/guards/ip-access.guard';
 import { IpAccess } from '../common/decorators/ip-access.decorator';
 import { EdgesService } from './edges.service';
 
-@UseGuards(JwtAuthGuard, IpAccessGuard)
+@UseGuards(IpAccessGuard)
 @Controller('ips/:ipId/edges')
 export class EdgesController {
   constructor(private readonly edges: EdgesService) {}
