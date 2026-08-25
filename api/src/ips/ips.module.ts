60eeae26d3c146d00aa20ea3f5525a04f26400c2
may6ixSong <138551769+may6ixSong@users.noreply.github.com>
2026-08-25 10:39:06 +0900

    Update ips.module.ts

--- api/src/ips/ips.module.ts
+++ api/src/ips/ips.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonAccessModule } from '../common/common-access.module';
-import { UsersModule } from '../users/users.module';
 import { AuditModule } from '../audit/audit.module';
 import { IpsService } from './ips.service';
 import { IpsController } from './ips.controller';
 
 @Module({
-  imports: [CommonAccessModule, UsersModule, AuditModule],
+  imports: [CommonAccessModule, AuditModule],
   providers: [IpsService],
   controllers: [IpsController],
   exports: [IpsService],
