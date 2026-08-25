938b016c463a9714b0446f3c540742f4f2f20a37
may6ixSong <138551769+may6ixSong@users.noreply.github.com>
2026-08-25 10:34:20 +0900

    Create current-actor.decorator.ts

---
+++ api/src/common/decorators/current-actor.decorator.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Actor, resolveActor } from '../actor';
+
+/** X-Knox-Id 헤더에서 호출자를 세운다. 헤더가 없으면 401. */
+export const CurrentActor = createParamDecorator((_: unknown, ctx: ExecutionContext): Actor => {
+  return resolveActor(ctx.switchToHttp().getRequest());
+});
