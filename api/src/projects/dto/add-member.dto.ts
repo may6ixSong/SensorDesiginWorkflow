db684c2ce4d8bf6a80d1ac0b0f638c9566b88429
may6ixSong <138551769+may6ixSong@users.noreply.github.com>
2026-08-25 10:40:11 +0900

    Update add-member.dto.ts

--- api/src/projects/dto/add-member.dto.ts
+++ api/src/projects/dto/add-member.dto.ts
@@ -1,9 +1,12 @@
-import { IsMongoId, IsString } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
+/** 사용자는 KnoxID로 지정한다 (api에는 users 컬렉션이 없다 - src/common/actor.ts). */
 export class AddMemberDto {
-  @IsMongoId()
-  userId: string;
+  @IsString()
+  @MinLength(1)
+  knoxId: string;
 
   @IsString()
+  @MinLength(1)
   department: string;
 }
